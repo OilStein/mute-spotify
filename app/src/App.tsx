884d6783efe5020fee2006d13dee357ce9ac5636
Niko Win <nikobranart@gmail.com>
2023-02-01 22:05:28 +0200

    App created. Components sketches

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import { createSignal } from "solid-js";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./styles/App.css";
+
+function App() {
+
+  return (
+  <div>
+  </div>
+);
+}
+
+export default App;
---
+++ app/src/components/SidebarOption.tsx
@@ -0,0 +1,19 @@
+import "./styles/SidebarOption.css";
+
+import {Component} from 'solid-js';
+
+type SidebarOptionProps = {
+  title: any,
+  Icon: any,
+}
+
+const SidebarOption: Component<SidebarOptionProps> = ({title, Icon}) => {
+    return (
+      <div class="sidebarOption">
+        {Icon && <Icon className="sidebarOption__icon" />}
+        {Icon ? <h4>{title}</h4> : <p>{title}</p>}
+      </div>
+    )
+}
+
+export default SidebarOption;
